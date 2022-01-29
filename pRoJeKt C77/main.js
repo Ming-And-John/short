2aca78b21b215dbb48b471f569a1683e3d4897b3
Ming-And-John <90568312+Ming-And-John@users.noreply.github.com>
2022-01-29 11:43:19 -0500

    Add files via upload

---
+++ pRoJeKt C77/main.js
@@ -0,0 +1,9 @@
+var firstname="john";
+     var lastname="sync";
+     console.log (lastname);
+var fullname="firstname.concat"
+console.log (fullname);
+var lengthoffullname=fullname.length;
+console.log (lengthoffullname);
+var name1="I am John";
+console.log (name1);
